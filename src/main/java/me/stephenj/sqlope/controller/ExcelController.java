dc96e7df7354c23ea4b89c72ab440ff68d03fc4b
Stephen Evenson <1258455347@qq.com>
2020-10-24 19:23:06 +0800

    添加Excel导入

--- src/main/java/me/stephenj/sqlope/controller/ExcelController.java
+++ src/main/java/me/stephenj/sqlope/controller/ExcelController.java
@@ -2,21 +2,22 @@ package me.stephenj.sqlope.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import me.stephenj.sqlope.Exception.ConditionsException;
 import me.stephenj.sqlope.Exception.DatabaseNotExistException;
 import me.stephenj.sqlope.Exception.TableNotExistException;
 import me.stephenj.sqlope.common.api.CommonResult;
+import me.stephenj.sqlope.common.utils.LogGenerator;
 import me.stephenj.sqlope.domain.TbTemp;
 import me.stephenj.sqlope.service.ExcelService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -35,6 +36,8 @@ public class ExcelController {
     private static final Logger LOGGER = LoggerFactory.getLogger(TbController.class);
     @Autowired
     private ExcelService excelService;
+    @Autowired
+    private LogGenerator logGenerator;
     @ApiOperation("导出Excel")
     @RequestMapping(value = "/export", method = RequestMethod.POST)
     @ResponseBody
@@ -49,5 +52,29 @@ public class ExcelController {
         response.setContentType("application/force-download");// 设置强制下载不打开
         response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);
         return CommonResult.success("导出Excel表格成功");
+    }
+
+    @ApiOperation("导入Excel")
+    @RequestMapping(value = "/import", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult importExcel(@RequestBody TbTemp tbTemp,
+                                    @RequestParam(value = "file")
+                                    @ApiParam("Excel文件") MultipartFile file,
+                                    HttpServletRequest request) {
+        int count = 0;
+        try {
+            count = excelService.importExcel(tbTemp, file);
+        } catch (TableNotExistException | DatabaseNotExistException | IOException e) {
+            LOGGER.error("import Excel failed:{}", e.getMessage());
+            return CommonResult.failed(e.getMessage());
+        }
+        if (count == 1) {
+            logGenerator.log(request, "给表格" + tbTemp.getName() + "导入Excel数据");
+            LOGGER.info("import excel success");
+            return CommonResult.success(tbTemp);
+        } else {
+            LOGGER.error("import excel failed: 执行sql语句失败");
+            return CommonResult.failed("import Excel failed: 执行sql语句失败");
+        }
     }
 }
--- src/main/java/me/stephenj/sqlope/service/ExcelService.java
+++ src/main/java/me/stephenj/sqlope/service/ExcelService.java
@@ -4,10 +4,14 @@ import me.stephenj.sqlope.Exception.ConditionsException;
 import me.stephenj.sqlope.Exception.DatabaseNotExistException;
 import me.stephenj.sqlope.Exception.TableNotExistException;
 import me.stephenj.sqlope.domain.TbTemp;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletOutputStream;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public interface ExcelService {
     String exportExcel(TbTemp tbTemp, ServletOutputStream out) throws DatabaseNotExistException, SQLException, ConditionsException, TableNotExistException;
+
+    int importExcel(TbTemp tbTemp, MultipartFile file) throws DatabaseNotExistException, TableNotExistException, IOException;
 }
